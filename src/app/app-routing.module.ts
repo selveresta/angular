829e725e0c97d257a3fe0e8e81b4bf9411b3bc42
selveresta <artem.lypnyk@softsculptor.net>
2023-06-01 13:30:38 +0300

    seccond commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ExchangerComponent} from "./pages/exchanger/exchanger.component";
+import {HomeComponent} from "./pages/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'exchange', component: ExchangerComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExchangeComponent } from './components/exchange/exchange.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ExchangerComponent } from './pages/exchanger/exchanger.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CurrencyHeaderComponent } from './components/currency-header/currency-header.component';
+import {HttpClientModule} from "@angular/common/http";
+import { OneCurrencyComponent } from './components/one-currency/one-currency.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExchangeComponent,
+    NavbarComponent,
+    ExchangerComponent,
+    FooterComponent,
+    HomeComponent,
+    CurrencyHeaderComponent,
+    OneCurrencyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
